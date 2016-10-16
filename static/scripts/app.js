ab5b035d6a2544af19a3a1280b0da57659d5413e
Scott Smith <mail@scotthsmith.com>
2016-10-16 06:35:55 -0400

    Front-endyness

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -1,1 +1,18 @@
 boomsvgloader.load('/static/images/icons/icon-sprite.svg');
+
+document.addEventListener( 'DOMContentLoaded', function() {
+	
+	var elem = document.querySelector('.articles');
+	
+	// Setup Masonry.
+	var msnry = new Masonry( elem, {
+		itemSelector: '.article',
+		transitionDuration: 0
+	});
+	
+	imagesLoaded( elem ).on( 'progress', function() {
+		// Layout Masonry after each image loads.
+		msnry.layout();
+	});
+
+});
