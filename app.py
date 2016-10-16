22dc66e972d098117b0e6dce761c44be933f7eed
Christopher Thompson <chrislegolife@gmail.com>
2016-10-16 03:36:27 -0700

    Added stock info

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask, request
 from flask import render_template
+import json
 
 from goose import Goose
 from core.models import *
 from newspaper import Article as NewspaperArticle
 import sqlalchemy
 from sqlalchemy.sql.expression import extract
+
+import ystockquote
 
 from api import get_search_results, get_sentiment, get_textblob_sentiment
 
@@ -38,6 +41,15 @@ def get_content():
         db.session.commit()
     return "done"
 
+# Example usage: http://localhost:5000/stock/AAPL?startdate=2015-01-01&enddate=2016-10-01
+@app.route('/stock/<company>', methods=['GET'])
+def get_stock(company):
+    start_date = request.args.get("startdate")
+    end_date = request.args.get("enddate")
+
+    stock_data = ystockquote.get_historical_prices(company, start_date, end_date)
+
+    return json.dumps([[x, y["Close"]] for x, y in stock_data.iteritems()])
 
 @app.route('/sentiment/<company>', methods=['GET'])
 def get_sentiment(company):
@@ -48,49 +60,16 @@ def get_sentiment(company):
 
 @app.route('/sentiment/graph/<company>', methods=['GET'])
 def get_sentiment_graph(company):
-    article_groups = Article.query.group_by(extract("day", Article.ArticlePublishDate)).all()
-    for articles in article_groups:
-        print articles
 
-        """
-    if article.ArticleContent == '':
-        print article.ArticleID
-        try:
-            current = g.extract(url=article.ArticleURL)
-            article.ArticleContent = current.cleaned_text
-            db.session.commit()
-        except:
-            try:
-                current = Article(article.ArticleURL)
-                current.download()
-                article.ArticleContent = current.text
-                db.session.commit()
-            except:
-                print "couldn't parse it"
-    """
-    """
-    articles = Article.
-    for article in articles:
-        thistime = article.ArticlePublishDate
-        rowsdict[Product.query.get(row['product_id']).title][int(time.mktime(thistime.timetuple())) * 1000] = row[
-            'count']
-        # hack for rows with 0 calls, initalize the previous and next hour if it doesn't exist
-        # we are abusing the beautiful defaultdict here.
-        # dont let the feminists know.
-        rowsdict[Product.query.get(row['product_id']).title][
-            int(time.mktime((thistime - timedelta(days=1)).timetuple())) * 1000]
-        if not (thistime + timedelta(hours=1)) > datetime.now():  # no time travel allowed
-            rowsdict[Product.query.get(row['product_id']).title][
-                int(time.mktime((thistime + timedelta(days=1)).timetuple())) * 1000]
-    # extend the graph to now with null values
-    # highcharts wants sorted data
-    rowsdict = OrderedDict(sorted(rowsdict.items(), key=lambda x: x[0]))
-    series = []
-    for prod, datapoints in rowsdict.iteritems():
-        series.append({'name': prod, 'data': sorted(datapoints.items(), key=lambda x: x[0])})
-    return json.dumps(series)
-    """
-    return render_template("demo.html")
+    highcharts = [{"data":[]}]
+    for x in db.session.execute('''SELECT  date(a.ArticlePublishDate), avg(a.articleScore) from ARTICLE a
+     	group by date(a.ArticlePublishDate)
+     	order by date(a.ArticlePublishDate) asc
+     	'''.format(company)):
+
+        highcharts[0]["data"].append([x[0], x[1]])
+
+    return json.dumps(highcharts)
 
 
 @app.route('/', methods=['GET', 'POST'])
